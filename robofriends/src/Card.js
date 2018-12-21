4700fb41e86b6af5eb4a6de0a3bc2cf1e0a0cc67
yuberalberto <yuberalberto@gmail.com>
2018-12-21 18:34:22 -0500

    Changes implemented until the class 188

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-export const Card = ({ id, name, email}) =>{
+// export const Card = ({ id, name, email}) =>{
+const Card = ({ id, name, username, email}) =>{
+
 
     return (
         // <h1>RoboFriend</h1>
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+            <img alt='robots' src={`https://robohash.org/${id}`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
@@ -14,4 +16,4 @@ export const Card = ({ id, name, email}) =>{
     );
 }
 
-// export default Card;
+export default Card;
