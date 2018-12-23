44a4689fe3b52ad8b225929f6f9601a9d96cc7a9
yuberalberto <yuberalberto@gmail.com>
2018-12-23 03:55:11 -0500

    app finished up til class 191

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 ReactDOM.render( 
-<App/>
+<App />
 , document.getElementById('root'));
 
 
